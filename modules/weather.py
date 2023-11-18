8711039d5f242d5f5005b0dff2be9c793595f435
real-zephex <zephex-1@outlook.com>
2023-11-18 07:01:32 +0530

    Added caching to the weather module. Can even release it as a module now

--- modules/weather.py
+++ modules/weather.py
@@ -1,6 +1,8 @@
+import os
+import json
 import requests
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Weather:
     """
@@ -28,25 +30,80 @@ class Weather:
         self.city = city.capitalize()
         self.city_url = f"http://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.apikey}"
 
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        self.cache_folder = os.path.join(script_dir, "cache")
+
         logging.basicConfig(level=logging.INFO)
 
     def make_request(self):
         """
-        Makes a request to the OpenWeatherMap API and retrieves the weather information.
+        Makes a request to the OpenWeatherMap API and retrieves the weather information. Also checks whether cache is present for that city or not
+
+        Updates:
+            Implemented a cache system, so that less requests are made to the API therefore reducing the number of API calls
 
         Returns:
             dict or None: A dictionary containing weather information if successful, None otherwise.
         """
+        cached_data = self.load_cache_file()
+        if cached_data and self.is_cached_valid(cached_data):
+            return cached_data["data"]
+
         try:
             response = requests.get(self.city_url)
             response.raise_for_status()
+            self.save_cached_data(response.json())
             return response.json()
         except requests.exceptions.RequestException as e:
-            logging.error(f"Error making requests: {e}")
+            logging.error(f"Network error making requests: {e}")
             return None
         except ValueError as e:
             logging.error(f"Error parsing JSON: {e}")
             return None
+        
+    def load_cache_file(self):
+        """
+        Checks whether the cached data is available or not.
+
+        Returns:
+            json or None: A json file containing all the information about the weather.
+        """
+        cache_file = os.path.join(self.cache_folder, f"{self.city}_cache.json")
+        
+        if os.path.exists(cache_file):
+            with open(cache_file, "r") as file:
+                return json.load(file)
+        return None
+    
+
+    def save_cached_data(self, data):
+        """
+        Saves the information for a city in a json file. <city_name>_cache.json is the file name.
+
+        Returns:
+            Nothing: It saves the file so there's no need to return anything.
+        """
+        cache_file = f"{self.city}_cache.json"
+
+        timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
+        cached_data = {"timestamp": timestamp, "data": data}
+
+        if not os.path.exists(self.cache_folder):
+            os.mkdir("cache")
+
+        with open(os.path.join(self.cache_folder, cache_file), "w") as file:
+            json.dump(cached_data, file)
+
+    def is_cached_valid(self, cached_data):
+        """
+        Checks whether the cached data is valid or not. Default time interval is 30 minutes, you can change it according to your need.
+
+        Returns:
+            True or False: True if the cached data is valid, False otherwise.
+        """
+        expiration = timedelta(hours = 0.5)
+        timestamp = datetime.strptime(cached_data["timestamp"], "%Y-%m-%dT%H:%M:%S")
+        return datetime.utcnow() - timestamp < expiration
 
     def coordinates(self):
         """
@@ -141,5 +198,3 @@ class Weather:
                 logging.error("Error getting sun information.")
         else:
             return None
-
-# print(Weather("toroo").coordinates())
